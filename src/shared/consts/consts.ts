5f551ed511a1929229e3c1bf9dffb47167c36e8b
AlexeyBMSTU <nap8099@gmail.com>
2024-12-29 00:55:21 +0300

    Вынес интерфейсы в отдельную папку

--- src/shared/consts/consts.ts
+++ src/shared/consts/consts.ts
@@ -1,21 +1,8 @@
-
-interface ApiMethod {
-    method: 'GET' | 'POST' | 'DELETE';
-    url: string,
-    body?: any
-}
-
-interface DocumentationServices {
-    home: {
-        getImages: ApiMethod,
-    }
-}
-
 const documentationServices: DocumentationServices = {
     home:  {
         getImages: {
             method: 'GET',
-            url: `/api/home/images`,
+            url: '/api/home/images',
         }
     }
 }
---
+++ src/shared/interfaces/interfaces.ts
@@ -0,0 +1,11 @@
+interface ApiMethod {
+    method: 'GET' | 'POST' | 'DELETE';
+    url: string,
+    body?: any
+}
+
+interface DocumentationServices {
+    home: {
+        getImages: ApiMethod,
+    }
+}
